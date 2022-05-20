27ce82b6fb8429298faa3db0e562fbcbd91a5f6b
NicolaasZA <lpretorius1995@gmail.com>
2022-05-20 10:10:50 +0200

    fixes

--- www/androidimei.js
+++ www/androidimei.js
@@ -1,25 +1,25 @@
 cordova.define("cordova-plugin-ia-imei.AndroidIMEI", function (require, exports, module) {
 	"use strict";
-	function Launcher() { }
+	function AndroidIMEI() { }
 
 	const emptyCallback = () => { };
 
-	Launcher.prototype.imei = function (successCallback, errorCallback) {
+	AndroidIMEI.prototype.imei = function (successCallback, errorCallback) {
 		options = {};
 		successCallback = successCallback || emptyCallback;
 		errorCallback = errorCallback || emptyCallback;
 		cordova.exec(successCallback || null, errorCallback || null, "AndroidIMEI", "getDeviceId", [options]);
 	};
 
-	Launcher.install = function () {
+	AndroidIMEI.install = function () {
 		if (!window.plugins) {
 			window.plugins = {};
 		}
 
-		window.plugins.launcher = new Launcher();
-		return window.plugins.launcher;
+		window.plugins.androidimei = new AndroidIMEI();
+		return window.plugins.androidimei;
 	};
 
-	cordova.addConstructor(Launcher.install);
+	cordova.addConstructor(AndroidIMEI.install);
 
 });
