86d98b5437051813374f5ed6f77f28642bb18d91
NicolaasZA <lpretorius1995@gmail.com>
2022-05-20 10:11:40 +0200

    clean

--- www/androidimei.js
+++ www/androidimei.js
@@ -2,13 +2,8 @@ cordova.define("cordova-plugin-ia-imei.AndroidIMEI", function (require, exports,
 	"use strict";
 	function AndroidIMEI() { }
 
-	const emptyCallback = () => { };
-
 	AndroidIMEI.prototype.imei = function (successCallback, errorCallback) {
-		options = {};
-		successCallback = successCallback || emptyCallback;
-		errorCallback = errorCallback || emptyCallback;
-		cordova.exec(successCallback || null, errorCallback || null, "AndroidIMEI", "getDeviceId", [options]);
+		cordova.exec(successCallback || null, errorCallback || null, "AndroidIMEI", "getDeviceId", []);
 	};
 
 	AndroidIMEI.install = function () {
